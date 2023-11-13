97dd597dc9b809ec0d22c7b270ab491385cc51e5
Robert <robert_beraru@yahoo.com>
2023-11-13 15:09:23 +0200

    Add cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "9opyd7",
+  defaultCommandTimeout: 15000,
+  pageLoadTimeout: 15000,
+  requestTimeout: 15000,
   e2e: {
     setupNodeEvents(on, config) {
   
